a21964efd898aacb516155abcf16e97f7c0439bd
Giovanni Moreira <2500078@salas.aulas>
2026-02-23 21:24:38 -0300

    fix: arrumando local do arquivo

---
+++ ATV praticas/aula2.sql
@@ -0,0 +1,37 @@
+-- 📘 Lista de Exercícios – DML (Banco DB_GERAL)
+-- 🔹 Questão 1 – INSERT
+-- Pergunta:
+-- Insira um novo cliente chamado Mariana Lopes, residente em São Paulo – SP.
+
+-- 🔹 Questão 2 – INSERT
+-- Pergunta:
+-- Cadastre um novo vendedor com faixa de comissão B e salário R$ 4.200,00.
+
+
+-- 🔹 Questão 3 – INSERT
+-- Pergunta:
+-- Cadastre um novo produto chamado Webcam Full HD no valor de R$ 299,90.
+
+
+-- 🔹 Questão 4 – INSERT com FK
+-- Pergunta:
+-- Crie um novo pedido para o cliente 51, atendido pelo vendedor 1, com prazo de entrega de 7 dias.
+
+
+-- 🔹 Questão 5 – INSERT em tabela associativa
+-- Pergunta:
+-- Adicione dois itens ao pedido 1101.
+
+
+-- 🔹 Questão 6 – UPDATE
+-- Pergunta:
+-- Atualize o endereço do cliente 51 para Avenida Central, 500.
+
+-- 🔹 Questão 7 – UPDATE com WHERE
+-- Pergunta:
+-- Aumente em 10% o salário dos vendedores da faixa de comissão A.
+
+
+-- 🔹 Questão 8 – UPDATE com JOIN
+-- Pergunta:
+-- Atualize o prazo de entrega para 5 dias em todos os pedidos feitos por clientes do estado de SP.
